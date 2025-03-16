9fa00a866dbc6153699cbe3f6a29713ec0b3db82
Saif6767 <saifk000000002@gamil.com>
2025-03-16 14:37:17 +0530

    fix: updated dependencies and TaskBoard component

--- Frontend/src/components/TaskBoard.jsx
+++ Frontend/src/components/TaskBoard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import TaskCard from "../components/Card"; // 
 
 const TaskBoard = ({ tasks, setTasks, handleEditTask, handleDeleteTask }) => {
